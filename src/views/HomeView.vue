42863f72ac2edb4e2a88553206cf39d05773df7f
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-04-28 19:05:05 -0300

    Added horizontal scroll on mouse down

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import CardModal from '@/components/Card/Modal/CardModal.vue'
 import Column from '../components/Column/Column.vue'
 import NewColumn from '../components/Column/NewColumn.vue'
 import { useColumnsStore } from '@/stores/columns'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { storeToRefs } from 'pinia'
 import { useCardsStore } from '@/stores/cards'
@@ -14,6 +14,11 @@ const route = useRoute()
 const columnsStore = useColumnsStore()
 const cardsStore = useCardsStore()
 const { columns } = storeToRefs(columnsStore)
+const isDown = ref(false)
+const startX = ref(null)
+const scrollLeft = ref(null)
+const scrollTop = ref(null)
+const container = ref(null)
 
 const cardExists = computed(() =>
   cardsStore.cards.find((card) => card.id === Number(route.params.id))
@@ -27,12 +32,38 @@ onMounted(() => {
 const onDrop = ({ removedIndex, addedIndex }) => {
   if (removedIndex !== addedIndex) columnsStore.moveColumn(removedIndex, addedIndex)
 }
+
+const mouseDown = (evt) => {
+  isDown.value = true
+  startX.value = evt.pageX - container.value.$el.offsetLeft
+  scrollLeft.value = container.value.$el.scrollLeft
+  scrollTop.value = container.value.$el.scrollTop
+}
+const mouseUp = () => {
+  isDown.value = false
+}
+const mouseLeave = () => {
+  isDown.value = false
+}
+const mouseMove = (evt) => {
+  if (isDown.value) {
+    evt.preventDefault()
+    const x = evt.pageX - container.value.$el.offsetLeft
+    const walkX = x - startX.value
+    container.value.$el.scrollLeft = scrollLeft.value - walkX
+  }
+}
 </script>
 
 <template>
   <main class="h-[calc(100vh-3rem)] bg-blue-500">
     <CardModal v-if="showCardModal" />
     <Container
+      ref="container"
+      @mousedown="mouseDown"
+      @mouseup="mouseUp"
+      @mouseleave="mouseLeave"
+      @mousemove="mouseMove"
       orientation="horizontal"
       drag-handle-selector=".column-dnd"
       @drop="onDrop"
