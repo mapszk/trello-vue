ce073ceea7163d508d60a669eae47b386cb7d11a
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2024-07-15 15:13:09 -0300

    Fix

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -2,6 +2,13 @@
 import { Draggable } from 'vue3-smooth-dnd'
 import Tag from '../Tag.vue'
 import Avatar from '@/components/Avatar.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToCard = () => {
+  router.push({ name: 'card', params: { id: props.id } })
+}
 
 const props = defineProps({
   id: Number,
@@ -17,7 +24,7 @@ const props = defineProps({
   <Draggable
     class="bg-white shadow-sm cursor-pointer text-left rounded-xl hover:outline outline-2 outline-blue-500"
   >
-    <RouterLink :to="{ name: 'card', params: { id: props.id } }">
+    <div @click="goToCard">
       <header v-if="color" class="h-10 rounded-t-xl" :style="{ backgroundColor: color }"></header>
       <div
         :class="[color ? 'rounded-b-xl' : 'rounded-xl', 'p-2 border border-gray-300 border-t-none']"
@@ -30,7 +37,7 @@ const props = defineProps({
         </h4>
         <Avatar class="w-7 h-7 ml-auto text-gray-400" />
       </div>
-    </RouterLink>
+    </div>
   </Draggable>
 </template>
 
